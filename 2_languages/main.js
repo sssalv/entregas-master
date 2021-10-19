ecee9a51e7728ee41d6e44c64608f7903bcacccb
Jaime Zapata <psysalv@gmail.com>
2021-10-19 20:59:54 +0200

    resolución ejercicio 1

--- 2_languages/main.js
+++ 2_languages/main.js
@@ -10,17 +10,31 @@
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/prototype
 
-function biggestWord(phrase) {
-    let splitMessage = phrase.split(' ')
-    for (let i = 0; i < splitMessage.length; i++) {
-        let element = splitMessage[i].length;
-        if (element){
+// function biggestWord(phrase) {
+//     var splitMessage = phrase.split(' ')
+//     var currentBiggest = 0
+//     for (i = 0; i < splitMessage.length; i++) {        
+//         if (splitMessage[i].length > currentBiggest){
+//             var currentBiggest = splitMessage[i].length
+//             biggest = splitMessage[i]
+//         }
+//     }
+//     return biggest
+// }
+// console.log(biggestWord('a bb ccc dddd eeeee'))
 
+function biggestWord(phrase) {
+    if (typeof phrase === 'string'){
+        let splitMessage = phrase.split(' ')
+        let currentBiggest = '';
+        for(let i = 0; i < splitMessage.length; i++){
+            if(currentBiggest < splitMessage[i]){
+                currentBiggest = splitMessage[i];
+            }
         }
-        
+        return currentBiggest;
+    }else{
+        return 'Dame una cadena porfa';
     }
 }
-biggestWord('no se por que estamos aqui')
-// // Ejemplo
-// console.log(biggestWord("Esta frase puede contener muchas palabras")); // "contener"
-// console.log(biggestWord("Ejercicios básicos de JavaScript")); // "Ejercicios"
+console.log(biggestWord('aaa bbb'));
