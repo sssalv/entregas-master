86c41695d3dfff965f167757f840421ece29f398
Jaime Zapata <psysalv@gmail.com>
2022-07-10 10:59:02 +0200

    message

--- 2_languages/3_clone_merge/scripts.js
+++ 2_languages/3_clone_merge/scripts.js
@@ -1,19 +1,60 @@
+// CLONE
 obj = {
     podriamos: 'podriamos',
     haber: 'haber',
     podido: 'podido',
-    funcionar: (e) => {console.log(e*2)}
 }
 
-const clone = (source) => {
+const cloneViaAssign = (source) => {
     let newObj = {}
     Object.assign(newObj, source)
-    
-    console.log('El objeto original es:',obj)
-    console.log('El objeto nuevo es:',newObj)
 
-    newObj.funcionar = (e) => {console.log(e*3)}
-    obj.funcionar(3)
-    newObj.funcionar(3)
+    console.log('[ObjectAssign Method] El objeto original es:',obj)
+    console.log('[ObjectAssign Method] El objeto nuevo es:',newObj)
 }
-clone(obj)
+cloneViaAssign(obj)
+
+const cloneViaSpread = (source) => {
+    let newObj = { ...source}
+
+    console.log('[Spread Method] El objeto original es:',obj)
+    console.log('[Spread Method] El objeto nuevo es:',newObj)
+}
+cloneViaSpread(obj)
+
+const cloneViaJson = (source) => {
+    let newObj = JSON.parse(JSON.stringify(source));    
+
+    console.log('[JSON Method] El objeto original es:',obj)
+    console.log('[JSON Method] El objeto nuevo es:',newObj)
+}
+cloneViaJson(obj)
+
+const cloneViaStructuredClone = (source) => {
+    let newObj = structuredClone(source)
+
+    console.log('[StructuredClone Method] El objeto original es:',obj)
+    console.log('[StructuredClone Method] El objeto nuevo es:',newObj)
+}
+cloneViaStructuredClone(obj)
+
+// MERGE
+a = {
+    nombre: "Jaime",
+    apellidos: "Zapata",
+}
+b = {
+    apellidos: "López",
+    edad: "28"
+}
+
+const merge = (target,source) => {
+    //Por defecto se sobrescriben de derecha a izquierda,
+    //asi que intercambio el orden y siempre prevalecerá
+    //el primer argumento de la función:
+
+    const merged = {...source,...target} 
+    console.log(merged)
+}
+
+merge(a,b)
