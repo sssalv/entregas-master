5d67b09a0feb8b69106988c26cb76eb76591e742
sssalv <psysalv@gmail.com>
2022-10-06 16:55:26 +0200

    ts support

---
+++ 3_bundling/webpack.config.common.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+module.exports = {
+  resolve: {
+    extensions: ['.js','.jsx','.ts','.tsx']
+  },
+  entry: {
+    prod:["./src/scripts.tsx","./src/style.scss"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      scriptLoading: "blocking",
+      hash:"true"
+    })
+  ],
+};
